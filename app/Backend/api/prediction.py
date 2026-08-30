37ed22d212eb5c621ab59ff7aa88d2352d8a0988
saed-gpr <saedgholipour81@gmail.com>
2026-08-30 17:13:39 +0330

    feat(api): implement prediction router with lifespan model loading

---
+++ app/Backend/api/prediction.py
@@ -0,0 +1,40 @@
+from fastapi import APIRouter, HTTPException, status
+from contextlib import asynccontextmanager
+import joblib
+
+
+ml_models = {}
+
+@asynccontextmanager
+async def router_lifespan(router: APIRouter):
+    try:
+        model_path = "/ML/analysis/optimized_house_price_pipeline.pkl"
+        ml_models['house_predictor'] = joblib.load(model_path)
+        print('success!')
+    except Exception as e:
+        print(f'Error : {e}')
+
+    yield
+
+    ml_models.clear()
+    print('Memory is clean')
+
+
+router = APIRouter(
+    prefix='/prediction',
+    tags=['predict'],
+    lifespan=router_lifespan
+)
+
+# predict 
+@router.post('/')
+def predict(data: dict):
+    model = ml_models.get('house_predictor')
+
+    if model is None:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail= "The model is unavailable"
+            )
+
+    return {"status": "success", "message": "Model is ready to predict!"}
---
+++ app/Backend/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from api.prediction import router as prediction_router
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title='HomeValue-AI API',
+    description='API for House Price Prediction',
+    version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+app.include_router(prediction_router)
+
+@app.get('/')
+def read_root():
+    return {
+        'status' : 'active',
+        'message' : "Welcome to HomeValue-AI Backend"
+    }
